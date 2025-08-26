ab5a89b5a777949c329b3a106528871698ce3927
André B <93703661+iamBadox@users.noreply.github.com>
2025-08-26 17:37:50 +0200

    Initial Commit

---
+++ ProteinTracker/ProteinTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProteinTrackerApp.swift
+//  ProteinTracker
+//
+//  Created by André Baudeau on 2025-08-26.
+//
+
+import SwiftUI
+
+@main
+struct ProteinTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ProteinTrackerTests/ProteinTrackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  ProteinTrackerTests.swift
+//  ProteinTrackerTests
+//
+//  Created by André Baudeau on 2025-08-26.
+//
+
+import Testing
+@testable import ProteinTracker
+
+struct ProteinTrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
